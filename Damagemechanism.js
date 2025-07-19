9f40a1fe084d2b75675bae4a44e0cff57b126155
kayetavi <avijitkayet97@gmail.com>
2025-07-19 18:27:26 +0530

    Create Damagemechanism.js

---
+++ Damagemechanism.js
@@ -0,0 +1,16 @@
+// Damagemechanism.js
+const damageMechanismDetails = {
+  "1": {
+    title: "Sulfidation",
+    description: "Occurs in hydrocarbon services at elevated temperatures containing sulfur."
+  },
+  "2": {
+    title: "Wet H2S Damage",
+    description: "Includes Blistering, HIC, SOHIC, and SSC in wet H2S environments."
+  },
+  "3": {
+    title: "Creep and Stress Rupture",
+    description: "Time-dependent deformation under high temperature and stress."
+  },
+  // Add more entries...
+};
